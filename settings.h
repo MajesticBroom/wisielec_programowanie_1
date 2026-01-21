11b2d8d7c580e3411dd740421364e41b8552f9ef
Mateusz Gębka <mateuszgebka2@gmail.com>
2026-01-21 12:59:14 +0100

    settings

---
+++ settings.h
@@ -0,0 +1,12 @@
+//
+// Created by MG on 19/12/2025.
+//
+
+#ifndef WISIELEC_SETTINGS_H
+#define WISIELEC_SETTINGS_H
+
+#include "models.h"
+
+void changePlayerNickname (Player &player, const std::string &newNickname);
+
+#endif //WISIELEC_SETTINGS_H
